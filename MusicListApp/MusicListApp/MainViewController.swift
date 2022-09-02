c60a74c2f2ac737c9ba6fb4e89ffe5e634a4b382
Эван Крошкин <>
2022-09-02 17:59:56 +0300

    Initial project setup

---
+++ MusicListApp/MusicListApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicListApp
+//
+//  Created by Эван Крошкин on 2.09.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

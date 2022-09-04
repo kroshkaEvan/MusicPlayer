26adc3bb6458b28daf61888be1266e39938d6cc8
Эван Крошкин <>
2022-09-04 17:44:40 +0300

    Added: musicSlider and buttons

--- MusicListApp/MusicListApp/MainViewController.swift
+++ MusicListApp/MusicListApp/MainViewController.swift
@@ -43,8 +43,45 @@ class MainViewController: UIViewController {
         return label
     }()
     
+    private lazy var musicSlider: UISlider = {
+        let slider = UISlider()
+        return slider
+    }()
     
-
+    private lazy var playMusicButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "play"),
+                        for: .normal)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowRadius = 15
+        button.layer.shadowOpacity = 5
+        button.layer.cornerRadius = 40
+        button.tintColor = .white
+        button.backgroundColor = UIColor(red: 29/255,
+                                         green: 23/255,
+                                         blue: 38/255,
+                                         alpha: 1)
+        return button
+    }()
+    
+    private lazy var nextMusicButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "forward.end"),
+                        for: .normal)
+        button.backgroundColor = .clear
+        button.tintColor = .white
+        return button
+    }()
+    
+    private lazy var backwardMusicButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "backward.end"),
+                        for: .normal)
+        button.backgroundColor = .clear
+        button.tintColor = .white
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
@@ -61,7 +98,9 @@ class MainViewController: UIViewController {
                                        blue: 38/255,
                                        alpha: 1)
         
-        [songNameLabel, musicanNameLabel].forEach({view.addSubview($0)})
+        [songNameLabel, musicanNameLabel,
+         musicSlider, playMusicButton, backwardMusicButton,
+        nextMusicButton].forEach({view.addSubview($0)})
         
         songNameLabel.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(16)
@@ -75,6 +114,33 @@ class MainViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-22)
             make.top.equalTo(songNameLabel.snp.bottom).offset(5)
             make.height.equalTo(20)
+        }
+        
+        musicSlider.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-22)
+            make.top.equalTo(musicanNameLabel.snp.bottom).offset(42)
+        }
+        
+        playMusicButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(80)
+            make.width.equalTo(80)
+            make.top.equalTo(musicSlider.snp.bottom).offset(78)
+        }
+        
+        nextMusicButton.snp.makeConstraints { make in
+            make.leading.equalTo(playMusicButton.snp.trailing).offset(10)
+            make.height.equalTo(45)
+            make.width.equalTo(45)
+            make.centerY.equalTo(playMusicButton.snp.centerY)
+        }
+        
+        backwardMusicButton.snp.makeConstraints { make in
+            make.trailing.equalTo(playMusicButton.snp.leading).offset(-10)
+            make.height.equalTo(45)
+            make.width.equalTo(45)
+            make.centerY.equalTo(playMusicButton.snp.centerY)
         }
         
     }
